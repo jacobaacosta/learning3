821abf8edf8ce58534fbc479692ce8cf1471f227
100176905 <jacobacosta32@yahoo.com>
2019-10-02 15:49:24 -0500

    Octohorpes and what they do

--- Learning3.py
+++ Learning3.py
@@ -16,3 +16,24 @@ print("Roosters:", 100 - 25 * 3 % 4)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print("is it true that 3 + 2 < 5 - 7?")
 print(3 + 2 < 5 - 7)
+
+
+# variables
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+carsNotDriven = cars - drivers
+cars_driven = cars
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers/cars_driven
+
+print("There are", cars, "cars available")
+print("There are only", drivers, "drivers available.")
+print("There will be", carsNotDriven, "empty cars today")
+print("We can transport", carpool_capacity, "people today.")
+print("we have", passengers, "to carpool today.")
+print("we need to put about", average_passengers_per_car, "in each car.")
+
+
+
